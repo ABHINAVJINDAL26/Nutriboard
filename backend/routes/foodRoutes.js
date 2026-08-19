557e51439cfc35e570c6d0b832ff75f1e4cf2799
ABHINAVJINDAL26 <ABHINAVJINDAL26@users.noreply.github.com>
2026-08-19 11:46:20 +0530

    refactor: remove suggestion engine feature and related components

--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -7,6 +7,5 @@ router.get('/foods', foodController.getFoods);
 router.post('/foods', foodController.addFood);
 router.delete('/foods/:id', foodController.deleteFood);
 router.post('/goal', foodController.updateGoal);
-router.get('/suggestions', foodController.getSuggestions);
 
 module.exports = router;
